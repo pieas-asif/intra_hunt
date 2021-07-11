58887344979dace437c5d40d50bf72a12cbcda65
pieas-asif <pieas.asif4@gmail.com>
2021-07-12 02:32:05 +0600

    Navbar Added on structure.html

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home_page
+from .views import home_page, landing_page
 
 urlpatterns = [
+    path('landing/', landing_page, name='landing'),
     path('', home_page, name='home'),
 ]
