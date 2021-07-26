2c3123e0a31f2275004cf6a19a2a7b06c0e8c04a
pieas-asif <pieas.asif4@gmail.com>
2021-07-26 21:37:56 +0600

    Code Cleanup + Working Login system

--- authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import auth_home_view, register_user
-
-urlpatterns = [
-    path('register/', register_user, name="register"),
-    path('', auth_home_view, name="auth_home")
-]
--- authentication/views.py
+++ authentication/views.py
@@ -2,17 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 
 from authentication.forms import UserRegistrationForm
-from home.forms import NewsletterForm
-
-
-def auth_home_view(request):
-    return render(request, 'authentication/home.html')
 
 
 def register_user(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
-        newsletter_form = NewsletterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -20,9 +14,8 @@ def register_user(request):
             return redirect('home')
     else:
         form = UserRegistrationForm()
-        newsletter_form = NewsletterForm()
     context = {
         'form': form,
-        'newsletter_form': newsletter_form
+
     }
     return render(request, 'authentication/register.html', context)
